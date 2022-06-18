200a4229490754770c157d4f557eef0182c28242
Daria Calitis <60710963+dbcalitis@users.noreply.github.com>
2022-06-17 20:44:04 -0400

    Update pet.py
    Added 'pacing_around' and 'idle' functions

--- pet.py
+++ pet.py
@@ -4,6 +4,7 @@ screen_dimension = 240 # 240 pixels each side
 margin = 20 # Serves as a border around the pet
 
 hatched = False
+speed = 1
 
 # Animation Frames
 frame_duration = 10 # deciseconds
@@ -14,26 +15,28 @@ slime_front_startframe = 4
 slime_right_startframe = 8
 slime_left_startframe = 12
 
-def pace_around(positionX, positionY):
+def pace_around(positionX):
+    # Initializing variables
     direction = 0 # 0 = Left, 1 = Right
 
-    if (positionX + frame_dimension) >= screen_dimension:
+    if (positionX + frame_dimension) >= screen_dimension + margin:
         direction = 0 # Left
-    elif positionX <= screen_dimension:
+    elif positionX <= 0 + margin:
         direction = 1 # Right
     else:
         direction = random.randint(0, 1)
-    
+
     # Pacing towards left
     if direction == 0:
         end_point = random.randint(0 + margin, positionX)
+        current_animation = slime_left_startframe
     # Pacing towards right
     elif direction == 1: 
         end_point = random.randint(positionX, screen_dimension - margin)
+        current_animation = slime_right_startframe
 
-    return end_point
+    return current_animation, end_point
 
 def idle():
     # Returns the animation start frame and 
-    # the amount of times it should play it
-    return slime_front_startframe, random.randint(1,2)
+    return slime_front_startframe
